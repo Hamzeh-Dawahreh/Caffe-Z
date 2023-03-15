3dfc1ae6f2cb5a407c60328319b3355560dfa918
Dawahreh <HAMZA.MOHMMAD99@YAHOO.COM>
2023-03-15 17:57:22 +0300

    Updated

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-let name = prompt("Let us know your name :-)");
-let gender = prompt("What is your gender?");
+// let name = prompt("Let us know your name :-)");
+// let gender = prompt("What is your gender?");
 
-if (gender === "male") {
-  alert("Hello Mr " + name);
-} else if (gender === "female") {
-  alert("Hello Ms " + name);
-} else {
-  alert("Welcome");
-}
-prompt("Do you need a hot or cold drink?");
+// if (gender === "male") {
+//   alert("Hello Mr " + name);
+// } else if (gender === "female") {
+//   alert("Hello Ms " + name);
+// } else {
+//   alert("Welcome");
+// }
+// prompt("Do you need a hot or cold drink?");
 
-let order = prompt("Can you write me your order?");
+// let order = prompt("Can you write me your order?");
 
-alert("Your drink is being prepared");
+// alert("Your drink is being prepared");
 
-console.log(name + " orderd a " + order);
+// console.log(name + " orderd a " + order);
