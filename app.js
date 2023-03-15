16ed86bab77b72e52b795b7918d1ddd512141f0f
Dawahreh <HAMZA.MOHMMAD99@YAHOO.COM>
2023-03-15 16:38:45 +0300

    added new file

---
+++ app.js
@@ -0,0 +1,17 @@
+// let name = prompt("Kindly enter your name :-)");
+// let gender = prompt("What is your gender?");
+
+// if (gender === "male") {
+//   alert("Hello Mr " + name);
+// } else if (gender === "female") {
+//   alert("Hello Ms " + name);
+// } else {
+//   alert("Welcome");
+// }
+// prompt("Do you need a hot or cold drink?");
+
+// let order = prompt("Can you write me your order?");
+
+// alert("Your drink is being prepared");
+
+// console.log(name + " orderd a " + order);
