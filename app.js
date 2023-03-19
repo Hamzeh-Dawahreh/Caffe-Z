23de1176d267760181936069cd5031797cb861d6
Dawahreh <HAMZA.MOHMMAD99@YAHOO.COM>
2023-03-19 16:23:17 +0300

    added new file

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 let name = prompt("Let us know your name :-)");
 let gender = prompt("What is your gender?");
+let type;
 
+while (gender != "male" && gender != "female") {
+  gender = prompt("What is your gender?");
+}
 if (gender === "male") {
   alert("Hello Mr " + name);
 } else if (gender === "female") {
   alert("Hello Ms " + name);
-} else {
-  alert("Welcome");
 }
-prompt("Do you need a hot or cold drink?");
-
+type = prompt("Do you need a hot or cold drink?");
 let order = prompt("Can you write me your order?");
 
-alert("Your drink is being prepared");
-
+let array = [name, gender, type, order];
+for (let index = 0; index < array.length; index++) {
+  let element = array[index];
+  console.log(element);
+}
 console.log(name + " orderd a " + order);
