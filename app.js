337247800e3b87928593faf020a3a2a0287e1508
Dawahreh <HAMZA.MOHMMAD99@YAHOO.COM>
2023-03-21 16:47:47 +0300

    updated

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let name = prompt("Let us know your name :-)");
+let age = prompt("How old are you?");
 let gender = prompt("What is your gender?");
 let type;
 function gender1() {
@@ -21,3 +22,25 @@ for (let index = 0; index < array.length; index++) {
   console.log(element);
 }
 console.log(name + " orderd a " + order);
+
+let Customer = document.getElementById("hello");
+let div = document.createElement("div");
+let p = document.createElement("p");
+let ul = document.createElement("ul");
+let li1 = document.createElement("li");
+let li2 = document.createElement("li");
+let li3 = document.createElement("li");
+
+Customer.appendChild(div);
+div.appendChild(p);
+Customer.appendChild(ul);
+ul.appendChild(li1);
+ul.appendChild(li2);
+ul.appendChild(li3);
+console.log(Customer);
+
+p.textContent = name;
+li1.textContent = `Gender: ${gender}`;
+
+li2.textContent = `Age: ${age}`;
+li3.textContent = `drink: ${type}  ${order}`;
